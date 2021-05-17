bd62bcf9b1f7f0af4bcde26f3406c727f71ae1a8
Fridry <fredwin.ed@gmail.com>
2021-05-17 04:56:26 -0300

    Criação das rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,15 @@
 import { Router } from 'express'
 
-const router = Router()
+import UserController from './controllers/UserController'
 
-export default router
+const routes = Router()
+
+const userController = new UserController()
+
+routes.get('/user', userController.listUsers)
+routes.get('/user/:cpf', userController.listUser)
+routes.post('/user', userController.createUser)
+routes.put('/user/:cpf', userController.updateUser)
+routes.delete('/user/:cpf', userController.deleteUser)
+
+export default routes
